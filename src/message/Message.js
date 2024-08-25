83af65dd1768f18c1b71523a3d8903d21a009183
frmnxhken <frmnxhken@gmail.com>
2024-08-25 16:51:46 +0700

    Create base  Message validation

---
+++ src/message/Message.js
@@ -0,0 +1,16 @@
+export default class Message {
+    constructor(min, max) {
+        this.messages = {
+            required: "This field must be filled in",
+            email: "Email format incorrect",
+            number: "This field only accepts numbers",
+            alphabet: "This field only accepts letters",
+            max: `This field maximal ${max} characters.`,
+            min: `This field minimum ${min} characters.`,
+            lower: "This field must be lowercase",
+            upper: "This field must be uppercase",
+            integer: "This field must be integer",
+            float: "This field must be float"
+        }
+    }
+}
